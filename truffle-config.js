bbc25f437bfb3a5a5f6bee7b38a7fa479f29c01f
Hyerin1208 <fghjiff8@gmail.com>
2022-02-23 12:04:38 +0900

    election

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,40 @@
-const path = require("path");
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+// 계약이 배포될 계정 자격 증명
+const mnemonic = process.env.MNEMONIC;
+
+// API key of your Datahub account for Avalanche Fuji test network
+const APIKEY = process.env.APIKEY;
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
-    }
-  }
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+
+    // fuji network라는 것 같다.
+    fuji: {
+      provider: function () {
+        return new HDWalletProvider({
+          mnemonic,
+          providerOrUrl: `https://avalanche--fuji--rpc.datahub.figment.io/apikey/${APIKEY}/ext/bc/C/rpc`,
+          chainId: "0xa869",
+        });
+      },
+      network_id: "*",
+      gas: 3000000,
+      gasPrice: 470000000000,
+      skipDryRun: true,
+    },
+  },
+
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
 };
